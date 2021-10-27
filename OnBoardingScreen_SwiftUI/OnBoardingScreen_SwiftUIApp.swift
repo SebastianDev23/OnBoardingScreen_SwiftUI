b8f5a6020fdbed37d2bfe266bb2dd8eecd359763
Sebastian <s.kloesel@gmx.de>
2021-10-27 18:01:08 +0200

    Initial Commit

---
+++ OnBoardingScreen_SwiftUI/OnBoardingScreen_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingScreen_SwiftUIApp.swift
+//  OnBoardingScreen_SwiftUI
+//
+//  Created by Sebastian Klösel on 27.10.21.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingScreen_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
